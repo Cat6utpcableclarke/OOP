76f91c4c813a42b322dc318acbf1593aef61faa5
Cat6utpcableclarke <160460664+Cat6utpcableclarke@users.noreply.github.com>
2024-09-21 17:38:52 +0800

    try

--- Project/src/main/java/com/mycompany/project/Project.java
+++ Project/src/main/java/com/mycompany/project/Project.java
@@ -11,29 +11,31 @@ package com.mycompany.project;
 public class Project {
     
     public static void main(String[] args) {
-        Account a1 = new Account("Neto",23103439);
-        
-        a1.Display();
-         Account a2 = new Account("Talib",23103469);
-        
-        a1.deposit(500);
-        a1.Display();
-        a1.verify();
-        a1.Display();
-        a1.withdraw(200);
-        a1.Display();
-//       Vehicle car = new Vehicle (100,0,"black","vios","toyota","GAE6969", false);
-//       
-//        System.out.println(car);
-//       
-//        car.display();
+//        Account a1 = new Account("Neto",23103439);
 //        
-//        car.accelerate();
-////        while(car.getCurrspeed()<=car.getTopspeed()){
-////         car.accelerate();
-////        }
-//         System.out.println(car);
+//        a1.Display();
+//         Account a2 = new Account("Talib",23103469);
+//        
+//        a1.deposit(500);
+//        a1.Display();
+//        a1.verify();
+//        a1.Display();
+//        a1.withdraw(200);
+//        a1.Display();
+
+
+       Vehicle car = new Vehicle (100,0,"black","vios","toyota","GAE6969", false);
+       
+        System.out.println(car);
+       
+        car.display();
         
+        car.accelerate();
+        while(car.getCurrspeed()<car.getTopspeed()){
+         car.accelerate();
+        }
+         System.out.println(car);
+         car.display();
         
     }
 }
